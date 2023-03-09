f81897904d8992382a0f5de629ea4e8ea6b93053
zanereee <110903231+zanereee@users.noreply.github.com>
2023-03-09 14:57:11 +0800

    Established connection to database and configured spring

--- ORM/src/main/java/com/cpi/controller/Login.java
+++ ORM/src/main/java/com/cpi/controller/Login.java
@@ -1,12 +1,28 @@
 package com.cpi.controller;
 
-import javax.servlet.http.HttpServlet;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
-public class Login extends HttpServlet{
+import com.cpi.dao.UsersDao;
+import com.cpi.model.Users;
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 4555418477902645091L;
+@Controller
+public class Login {
 
+	@RequestMapping("Login")
+	public ModelAndView login (@RequestParam("username") String username, @RequestParam("password") String password)
+	{
+		ModelAndView mv = new ModelAndView();
+		
+		UsersDao dao = new UsersDao();
+		Users user = dao.getUser(username, password);
+		
+		mv.addObject("user", user);
+		mv.setViewName("dashboard.jsp");
+		
+		return mv;
+		
+	}
 }
