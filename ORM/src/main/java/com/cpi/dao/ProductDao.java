481ad7f9a113fe643d32f618898175361e3f4414
John Hill Dalisay <johnhilldalisay@gmail.com>
2023-03-09 12:34:25 +0800

    Initialize Product model and dao

--- ORM/src/main/java/com/cpi/dao/ProductDao.java
+++ ORM/src/main/java/com/cpi/dao/ProductDao.java
@@ -1,5 +1,63 @@
 package com.cpi.dao;
 
-public class ProductDao {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+import com.cpi.model.DBConnect;
+import com.cpi.model.Product;
+import com.cpi.model.Users;
+
+public class ProductDao {
+	private static final String dbUsername = "CALANDRIA";
+	private static final String dbPassword = "calandria";
+	private static final String server = "training-db.cosujmachgm3.ap-southeast-1.rds.amazonaws.com";
+	
+	public Product getProduct()
+	{
+		Product u = new Product();
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		
+		try {
+			
+			 DBConnect db = new DBConnect (server, "ORCL", dbUsername, dbPassword);
+			 conn = db.getConnection();
+			 System.out.println("Connected to server");
+			 
+			 st = conn.createStatement();
+			 rs = st.executeQuery("SELECT * FROM Product");
+			 
+			 if (rs.next())
+			 {
+				u.setProductID(rs.getInt("PRODUCT_ID"));
+			 	u.setProductName(rs.getString("PRODUCT_NAME"));
+			 	u.setProdcutDescription(rs.getString("PRODUCT_DESCRIPTION"));
+			 	u.setProductPicture(rs.getString("PRODUCT_PICTURE"));
+				u.setProductStatus(rs.getInt("PRODUCT_STATUS"));
+				u.setProductPrice(rs.getFloat("PRICE"));
+			 }
+			 
+		}  catch (SQLException se) {
+			System.out.println(se);
+		} finally {
+			
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (st != null) {
+					st.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException se) {
+				System.out.println(se);
+			}
+		}
+		return u;
+	}
 }
--- ORM/src/main/java/com/cpi/model/Product.java
+++ ORM/src/main/java/com/cpi/model/Product.java
@@ -1,5 +1,54 @@
 package com.cpi.model;
 
 public class Product {
-
+	private int productID;
+	private String productName;
+	private String prodcutDescription;
+	private String productPicture;
+	private int productStatus;
+	private float productPrice;
+	
+	public int getProductID() {
+		return productID;
+	}
+	public void setProductID(int productID) {
+		this.productID = productID;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getProdcutDescription() {
+		return prodcutDescription;
+	}
+	public void setProdcutDescription(String prodcutDescription) {
+		this.prodcutDescription = prodcutDescription;
+	}
+	public String getProductPicture() {
+		return productPicture;
+	}
+	public void setProductPicture(String productPicture) {
+		this.productPicture = productPicture;
+	}
+	public int getProductStatus() {
+		return productStatus;
+	}
+	public void setProductStatus(int productStatus) {
+		this.productStatus = productStatus;
+	}
+	public float getProductPrice() {
+		return productPrice;
+	}
+	public void setProductPrice(float productPrice) {
+		this.productPrice = productPrice;
+	}
+	@Override
+	public String toString() {
+		return "Product [productName=" + productName + ", prodcutDescription=" + prodcutDescription
+				+ ", productPicture=" + productPicture + ", productStatus=" + productStatus + ", productPrice="
+				+ productPrice + "]";
+	}
+	
 }
